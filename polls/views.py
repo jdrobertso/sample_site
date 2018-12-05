f2120088471e6e47ec6c67ea514a3631e29c69ad
Jeremy Robertson <jrobertson.88@gmail.com>
2018-12-05 17:03:49 -0600

    preparing to switch to generic views

--- polls/views.py
+++ polls/views.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from django.template import loader
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -21,8 +21,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
